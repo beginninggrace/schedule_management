b6118bbaaeb63d60084ee45d90cf5b092c96fc0b
si eun <tldms123455@naver.com>
2024-03-17 22:03:45 +0900

    :sparkles: feat : ExceptionHandler 추가

--- src/main/java/com/sparta/schedule_management/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sparta/schedule_management/exception/GlobalExceptionHandler.java
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler({NotExistsUserException.class, NotExistsTodoException.class,
-        NotExistsCommentException.class})
+        NotExistsCommentException.class, NotExistsPageException.class})
     public ResponseEntity<RestApiException> notExistsException(Exception e) {
-        return RestApiException.of(e.getMessage(), HttpStatus.BAD_REQUEST.value());
+        return RestApiException.of(e.getMessage(), HttpStatus.NOT_FOUND.value());
     }
 
     @ExceptionHandler({NotMatchedPasswordException.class, NotMatchedUserException.class})
---
+++ src/main/java/com/sparta/schedule_management/exception/NotExistsPageException.java
@@ -0,0 +1,5 @@
+package com.sparta.schedule_management.exception;
+
+public class NotExistsPageException extends RuntimeException {
+    public NotExistsPageException(String message) {super(message);}
+}
